69fbd9dd2b5985934b052e08952b964080451e76
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-08-03 21:03:48 -0400

    Add webdriver function - wait until element exists.

--- R/utils.R
+++ R/utils.R
@@ -63,3 +63,34 @@ e_val <- function() stop("The specified value cannot be extracted from this UUID
 #' process_dataset: Report value cannot be extracted with specified output format
 #' @rdname process_dataset_e
 e_fmt <- function() stop("The specified output format is not available for this value.")
+
+#' Helper functions for webdriver navigation
+#'
+#' @name webdriver
+NULL
+
+#' webdriver_wait_for_element: Wait until element is visible
+#' @param webdriver RSelenium server object.
+#' @param By Type of selector to use.
+#' @param value Value for selector.
+#' @param timeout Timeout in seconds.
+#' @rdname webdriver
+#' @export
+webdriver_wait_for_element <- function(webdriver, By, value, timeout) {
+  # record start time
+  start_time <- Sys.time()
+
+  # check for element
+  element <- NULL
+  while (is.null(element)) {
+    element <- tryCatch({webdriver$client$findElement(using = By, value = value)},
+                        error = function(e){NULL})
+    # check timeout
+    if (as.numeric(Sys.time() - start_time) > timeout) {
+      stop("Timeout exceeded while waiting for element to be visible.")
+    }
+  }
+
+  # return element
+  return(element)
+}
