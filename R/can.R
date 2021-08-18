6bc14ce92089d0c74dfa04b7c915b588bc2ed1f4
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-08-17 20:58:11 -0400

    Read provincial data from PHAC dataset.

--- R/can.R
+++ R/can.R
@@ -27,6 +27,83 @@ process_can <- function(uuid, val, fmt, ds,
   # process datasets
   switch(
     uuid,
+    "f7db31d0-6504-4a55-86f7-608664517bdb" = {
+      switch(
+        val,
+        "cases" = {
+          switch(
+            fmt,
+            "ts_prov" = {
+              ds %>%
+                dplyr::select(.data$prname, .data$date, .data$numtotal) %>%
+                dplyr::mutate(date = as.Date(.data$date)) %>%
+                dplyr::filter(.data$prname == phac_prov(prov)) %>%
+                dplyr::select(-.data$prname) %>%
+                dplyr::rename(value = .data$numtotal) %>%
+                helper_ts(loc = "prov", val, prov, convert_to_cum = FALSE)
+            },
+            e_fmt()
+          )
+        },
+        "mortality" = {
+          switch(
+            fmt,
+            "ts_prov" = {
+              ds %>%
+                dplyr::select(.data$prname, .data$date, .data$numdeaths) %>%
+                dplyr::mutate(date = as.Date(.data$date)) %>%
+                dplyr::filter(.data$prname == phac_prov(prov)) %>%
+                dplyr::select(-.data$prname) %>%
+                dplyr::rename(value = .data$numdeaths) %>%
+                helper_ts(loc = "prov", val, prov, convert_to_cum = FALSE)
+            },
+            e_fmt()
+          )
+        },
+        "recovered" = {
+          switch(
+            fmt,
+            "ts_prov" = {
+              ds %>%
+                dplyr::select(.data$prname, .data$date, .data$numrecover) %>%
+                dplyr::mutate(date = as.Date(.data$date)) %>%
+                dplyr::filter(.data$prname == phac_prov(prov)) %>%
+                dplyr::select(-.data$prname) %>%
+                dplyr::rename(value = .data$numrecover) %>%
+                helper_ts(loc = "prov", val, prov, convert_to_cum = FALSE)
+            },
+            e_fmt()
+          )
+        },
+        "testing" = {
+          match.arg(testing_type, c("n_people_tested", "n_tests_completed"))
+          switch(
+            fmt,
+            "ts_prov" = {
+              if (testing_type == "n_people_tested") {
+                ds %>%
+                  dplyr::select(.data$prname, .data$date, .data$numtested) %>%
+                  dplyr::mutate(date = as.Date(.data$date)) %>%
+                  dplyr::filter(.data$prname == phac_prov(prov)) %>%
+                  dplyr::select(-.data$prname) %>%
+                  dplyr::rename(value = .data$numtested) %>%
+                  helper_ts(loc = "prov", val, prov, convert_to_cum = FALSE)
+              } else if (testing_type == "n_tests_completed") {
+                ds %>%
+                  dplyr::select(.data$prname, .data$date, .data$numtests) %>%
+                  dplyr::mutate(date = as.Date(.data$date)) %>%
+                  dplyr::filter(.data$prname == phac_prov(prov)) %>%
+                  dplyr::select(-.data$prname) %>%
+                  dplyr::rename(value = .data$numtests) %>%
+                  helper_ts(loc = "prov", val, prov, convert_to_cum = FALSE)
+              }
+            },
+            e_fmt()
+          )
+        },
+        e_val()
+      )
+    },
     "fa3f2917-6553-438c-9a6f-2af8d077f47f" = {
       switch(
         val,
