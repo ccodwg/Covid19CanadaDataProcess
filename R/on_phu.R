f9ad2ac5f42a25975ff9cb6e92fe7a6c86b41091
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-09-29 22:19:44 -0400

    Add C, M, R for Northwestern (ON PHU). #12

--- R/on_phu.R
+++ R/on_phu.R
@@ -75,6 +75,72 @@ process_on_phu <- function(uuid, val, fmt, ds,
         e_val()
       )
     },
+    # Northwestern
+    "4c56a58b-0cb3-4d71-bafe-9fdb42e5c1d5" = {
+      hr <- "Northwestern"
+      switch(
+        val,
+        "cases" = {
+          switch(
+            fmt,
+            "hr_cum_current" = {
+              ds %>%
+                rvest::html_table() %>%
+                {.[[grep("Totals", .)[1]]]} %>%
+                dplyr::filter(.data$Region == "Totals") %>%
+                dplyr::pull(.data$Total) %>%
+                readr::parse_number() %>%
+                data.frame(
+                  value = .
+                ) %>%
+                helper_cum_current(loc = "hr", val, prov, date_current, hr)
+            },
+            e_fmt()
+          )
+        },
+        "mortality" = {
+          switch(
+            fmt,
+            "hr_cum_current" = {
+              ds %>%
+                rvest::html_table() %>%
+                {.[[grep("Deceased", .)[1]]]} %>%
+                dplyr::pull(.data[["\u200BDeceased"]]) %>% # note zero-width space
+                as.character() %>%
+                readr::parse_number() %>%
+                data.frame(
+                  value = .
+                ) %>%
+                helper_cum_current(loc = "hr", val, prov, date_current, hr)
+            },
+            e_fmt()
+          )
+        },
+        "recovered" = {
+          switch(
+            fmt,
+            "hr_cum_current" = {
+              resolved <- ds %>%
+                rvest::html_table() %>%
+                {.[[grep("Totals", .)[1]]]} %>%
+                dplyr::filter(.data$Region == "Totals") %>%
+                dplyr::pull(.data$Resolved) %>%
+                readr::parse_number()
+              deceased <- ds %>%
+                rvest::html_table() %>%
+                {.[[grep("Deceased", .)[1]]]} %>%
+                dplyr::pull(.data[["\u200BDeceased"]]) %>% # note zero-width space
+                as.character() %>%
+                readr::parse_number()
+              data.frame(value = resolved - deceased) %>%
+                helper_cum_current(loc = "hr", val, prov, date_current, hr)
+            },
+            e_fmt()
+          )
+        },
+        e_val()
+      )
+    },
     e_uuid()
   )
 }
