2174def1ac98fe6a66a31d6a795cafe759ebda30
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2023-06-20 18:43:37 -0400

    Update processing of AB webpage table
    - Previously, two rows were removed to retain zone info, now only one row is removed, otherwise data for Calgary are erroneously cropped
    - Note: this will be important to account for if processing historical versions of this table (e.g., see ccodwg/CovidTimelineCanada#108)

--- R/ab.R
+++ R/ab.R
@@ -267,7 +267,7 @@ process_ab <- function(uuid, val, fmt, ds,
                 rvest::html_table() %>%
                 `[[`(1) %>%
                 # filter to zone info
-                dplyr::slice(-c(1, 2)) %>%
+                dplyr::slice(-1) %>%
                 dplyr::transmute(
                   sub_region_1 = .data$Location,
                   value = readr::parse_number(.data$`Confirmed cases`)
@@ -285,49 +285,11 @@ process_ab <- function(uuid, val, fmt, ds,
                 rvest::html_table() %>%
                 `[[`(1) %>%
                 # filter to zone info
-                dplyr::slice(-c(1, 2)) %>%
+                dplyr::slice(-1) %>%
                 dplyr::transmute(
                   sub_region_1 = .data$Location,
                   value = readr::parse_number(.data$Deaths)
                 ) %>%
-                helper_cum_current(loc = "hr", val, prov, date_current)
-            },
-            e_fmt()
-          )
-        },
-        e_val()
-      )
-    },
-    "d3b170a7-bb86-4bb0-b362-2adc5e6438c2" = {
-      switch(
-        val,
-        "hospitalizations" = {
-          switch(
-            fmt,
-            "hr_cum_current" = {
-              ds %>%
-                rvest::html_table() %>%
-                `[[`(1) %>%
-                dplyr::filter(!grepl("In", .$Location)) %>%
-                dplyr::select(.data$Location, .data$`In\n\t\t\thospital**`) %>%
-                dplyr::transmute(sub_region_1 = .data$Location,
-                                 value = readr::parse_number(.data$`In\n\t\t\thospital**`)) %>%
-                helper_cum_current(loc = "hr", val, prov, date_current)
-            },
-            e_fmt()
-          )
-        },
-        "icu" = {
-          switch(
-            fmt,
-            "hr_cum_current" = {
-              ds %>%
-                rvest::html_table() %>%
-                `[[`(1) %>%
-                dplyr::filter(!grepl("In", .$Location)) %>%
-                dplyr::select(.data$Location, .data$`In intensive\n\t\t\tcare***`) %>%
-                dplyr::transmute(sub_region_1 = .data$Location,
-                                 value = readr::parse_number(.data$`In intensive\n\t\t\tcare***`)) %>%
                 helper_cum_current(loc = "hr", val, prov, date_current)
             },
             e_fmt()
